f393c24b3b135bc354f023aa4f6c4145e020f956
Diana Ayala Galvan <dianaayalag@gmail.com>
2022-05-21 22:25:03 -0500

    test cases added

--- ZemogaMobileTestTests/ZemogaMobileTestTests.swift
+++ ZemogaMobileTestTests/ZemogaMobileTestTests.swift
@@ -9,28 +9,150 @@ import XCTest
 @testable import ZemogaMobileTest
 
 class ZemogaMobileTestTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // MARK: Business Functions Tests
+    
+    func testPost_Favorite() throws {
+        let sut = Post.create(Post.self, id: -1)
+        // Check if post has a false favorite by default
+        XCTAssertFalse(sut.favorite, "Post favorite is not false")
+        // delete created post
+        try Post.deleteSingle(id: -1)
+        // save
+        try CoreData.save()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testPost_ToggleFavorite() throws {
+        let sut = Post.create(Post.self, id: -1)
+        // Check if post can toggle favorite from false to true
+        sut.toggleFavorite()
+        XCTAssertTrue(sut.favorite, "Toggle favorite FTT didn't work")
+        // and viceversa
+        sut.toggleFavorite()
+        XCTAssertFalse(sut.favorite, "Toggle favorite TTF didn't work")
+        // delete created post
+        try Post.deleteSingle(id: -1)
+        // save
+        try CoreData.save()
     }
+    
+    // MARK: API Tests
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testPostsAPI_getAll() throws {
+        let sut = PostsWS()
+        let expectation = self.expectation(description: "Wait for posts to arrive")
+        sut.fetchPosts { postsDTO in
+            // Finish expectation
+            expectation.fulfill()
+            let posts = postsDTO.toPosts
+            // Check if posts are arriving from the API
+            XCTAssertFalse(posts.isEmpty, "Posts are not arriving")
+            // Check if all posts are arriving from the API
+            XCTAssertEqual(posts.count, 100, "Posts are arriving incomplete")
+        } error: { errorMessage in
+            // Finish expectation
+            expectation.fulfill()
+            // If we get here is because we have an error
+            XCTAssertNil(errorMessage, errorMessage.localizedDescription)
+            // Test should fail even if the error is nil
+            XCTFail("Server returned Unknown error")
         }
+        // Wait for service to arrive
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testPostsAPI_deleteAll() throws {
+        let sut = PostsWS()
+        let expectation = self.expectation(description: "Wait for posts to be deleted")
+        sut.deleteAllPosts {
+            // Finish expectation
+            expectation.fulfill()
+            // Since the API doesn't really delete, we are only checking if there's a success, so the test should pass
+        } error: { errorMessage in
+            // Finish expectation
+            expectation.fulfill()
+            // If we get here is because we have an error
+            XCTAssertNil(errorMessage, errorMessage.localizedDescription)
+            // Test should fail even if the error is nil
+            XCTFail("Server returned Unknown error")
+        }
+        // Wait for service to arrive
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testPostsAPI_deleteSingle() throws {
+        let sut = PostsWS()
+        let expectation = self.expectation(description: "Wait for post to be deleted")
+        sut.deletePostWithId(id: 1) {
+            // Finish expectation
+            expectation.fulfill()
+            // Since the API doesn't really delete, we are only checking if there's a success, so the test should pass
+        } error: { errorMessage in
+            // Finish expectation
+            expectation.fulfill()
+            // If we get here is because we have an error
+            XCTAssertNil(errorMessage, errorMessage.localizedDescription)
+            // Test should fail even if the error is nil
+            XCTFail("Server returned Unknown error")
+        }
+        // Wait for service to arrive
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testDetailAPI_getUser() throws {
+        let sut = PostDetailWS()
+        let expectation = self.expectation(description: "Wait for user to arrive")
+        sut.fetchUser(userID: 1) { userDTO in
+            // Finish expectation
+            expectation.fulfill()
+            // Check if there's a user arriving
+            XCTAssertNotNil(userDTO, "User didn't arrive")
+            // Check if it's the right user with the right data
+            XCTAssertEqual(userDTO.id, 1, "User with wrong id")
+            XCTAssertEqual(userDTO.name, "Leanne Graham", "User with wrong name")
+            XCTAssertEqual(userDTO.email, "Sincere@april.biz", "User with wrong email")
+            XCTAssertEqual(userDTO.phone, "1-770-736-8031 x56442", "User with wrong phone")
+            XCTAssertEqual(userDTO.website, "hildegard.org", "User with wrong website")
+        } error: { errorMessage in
+            // Finish expectation
+            expectation.fulfill()
+            // If we get here is because we have an error
+            XCTAssertNil(errorMessage, errorMessage.localizedDescription)
+            // Test should fail even if the error is nil
+            XCTFail("Server returned Unknown error")
+        }
+        // Wait for service to arrive
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testDetailAPI_getComments() throws {
+        let sut = PostDetailWS()
+        let expectation = self.expectation(description: "Wait for comments to arrive")
+        sut.fetchComments(postId: 1) { commentsDTO in
+            // Finish expectation
+            expectation.fulfill()
+            let comments = commentsDTO.toComments
+            // Check if posts are arriving from the API
+            XCTAssertFalse(comments.isEmpty, "Comments for post with id 1 are not arriving")
+            // Check if all posts are arriving from the API
+            XCTAssertEqual(comments.count, 5, "Comments for post with id 1 are arriving incomplete")
+            // Check if it's the right data for first comment
+            let firstComment = comments.first
+            // Check if there's a comment
+            XCTAssertNotNil(firstComment, "First comment is nil")
+            XCTAssertEqual(firstComment!.id, 1, "Comment with wrong id")
+            XCTAssertEqual(firstComment!.postId, 1, "Comment with wrong post id")
+            XCTAssertEqual(firstComment!.body, "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium", "Comment with wrong body")
+        } error: { errorMessage in
+            // Finish expectation
+            expectation.fulfill()
+            // If we get here is because we have an error
+            XCTAssertNil(errorMessage, errorMessage.localizedDescription)
+            // Test should fail even if the error is nil
+            XCTFail("Server returned Unknown error")
+        }
+        // Wait for service to arrive
+        waitForExpectations(timeout: 5, handler: nil)
     }
 
 }
--- ZemogaMobileTestUITests/ZemogaMobileTestUITests.swift
+++ ZemogaMobileTestUITests/ZemogaMobileTestUITests.swift
@@ -1,41 +1,171 @@
 //
-//  Zemoga_Mobile_Test_iOSUITests.swift
-//  Zemoga Mobile Test iOSUITests
+//  ZemogaMobileTestUITests.swift
+//  ZemogaMobileTestUITests
 //
 //  Created by Diana Ayala on 5/18/22.
 //
 
 import XCTest
 
-class Zemoga_Mobile_Test_iOSUITests: XCTestCase {
+class ZemogaMobileTestUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    // MARK: MainView
+    
+    func testTrashButton() throws {
+        // Create and launch app
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Check if navigation bar exists
+        let navigationBar = app.navigationBars["Posts"]
+        XCTAssertTrue(navigationBar.exists, "Navigation bar doesn't exist")
+        // Check if trash button exits
+        let trashButton = navigationBar.buttons["trash"]
+        XCTAssertTrue(trashButton.exists, "Trash button doesn't exist")
+        // Tapping trash button should open alert
+        trashButton.tap()
+        // Check if alert exists
+        let deleteAlert = app.alerts["Delete"]
+        XCTAssertTrue(deleteAlert.exists, "Delete alert doesn't exist (1st time)")
+        // Check if cancel button is there, then tap
+        let cancelButton = deleteAlert.buttons["Cancel"]
+        XCTAssertTrue(cancelButton.exists, "Cancel button doesn't exist")
+        cancelButton.tap()
+        // Since we cancelled the action, the alert should not be there
+        XCTAssertFalse(deleteAlert.exists)
+        // Now let's check the delete action of the alert, so we should tap the trash button again
+        trashButton.tap()
+        // check if the delete alert is open
+        XCTAssertTrue(deleteAlert.exists, "Delete alert doesn't exist (2nd time)")
+        // Check if delete button is there, then tap
+        let deleteButton = deleteAlert.buttons["Delete"]
+        XCTAssertTrue(deleteButton.exists, "Delete button doesn't exist (2nd time)")
+        deleteButton.tap()
+        // Alert of successfully deleted or failed, should display
+        let deletedAlert = app.alerts["Deleted"]
+        // We should wait for the posts to be deleted
+        XCTAssertTrue(deletedAlert.waitForExistence(timeout: 2), "Deleted alert doesn't exist")
+        // Check if ok button is there, then tap
+        let okButton = deletedAlert.buttons["Ok"]
+        XCTAssertTrue(okButton.exists, "Ok button doesn't exist")
+        okButton.tap()
+        // Since we deleted everything, the table should be empty
+        let emptyTable = app.tables["Empty list"]
+        XCTAssertTrue(emptyTable.exists, "Empty table isn't there")
+        // Terminate app
+        app.terminate()
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testFavoritesSegmentedControl() {
+        // Create and launch app
+        let app = XCUIApplication()
+        app.launch()
+        // Check if table exists
+        let table = app.tables.firstMatch
+        XCTAssertTrue(table.exists, "Table doesn't exist")
+        // We should have at least one favorite element
+        let favoriteCell = table.cells.containing(.staticText, identifier:"sunt aut facere repellat provident occaecati excepturi optio reprehenderit").firstMatch
+        XCTAssertTrue(favoriteCell.exists, "Cell to favorite doesn't exist")
+        // Let's check if the cell has a favorite button and tap it
+        let favoriteButton = favoriteCell.buttons["favorite"]
+        XCTAssertTrue(favoriteButton.exists, "Favorite button doesn't exist")
+        // Tap button to make it favorite
+        favoriteButton.tap()
+        // Check if there's 1 segmented control
+        let segmentedControl = app.segmentedControls.firstMatch
+        XCTAssertTrue(segmentedControl.exists, "Segmented control doesn't exist")
+        // Check if the segmented control has a favorites filter button and tap it
+        let favoritesFilterButton = segmentedControl.buttons["Favorites"]
+        XCTAssertTrue(favoritesFilterButton.exists, "Segmented control exists, but doesn't have a Favorites button")
+        favoritesFilterButton.tap()
+        // Check if table was filtered correctly and we have our favorite cell, this test also verifies if the favorite button of the cell works
+        XCTAssertTrue(favoriteCell.exists, "Favorite cell doesn't exist")
+        // Terminate app
+        app.terminate()
+    }
+    
+    func testSwipeToDelete_deleteWithButton() {
+        // Create and launch app
+        let app = XCUIApplication()
+        app.launch()
+        // Check if table exists
+        let table = app.tables.firstMatch
+        XCTAssertTrue(table.exists, "Table doesn't exist")
+        // Choose our cell to delete and check if exists
+        let cellToDelete = table/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"qui est esse")/*[[".cells.containing(.button, identifier:\"Delete\")",".cells.containing(.staticText, identifier:\"qui est esse\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch
+        XCTAssertTrue(cellToDelete.exists, "Cell to delete doesn't exist")
+        // Swipe left to delete
+        cellToDelete.swipeLeft()
+        // Check if button appeared
+        let deleteButton = cellToDelete.buttons["Delete"]
+        XCTAssertTrue(deleteButton.exists, "Delete button doesn't exist")
+        // Tap delete button
+        deleteButton.tap()
+        // Check if the cell was deleted
+        XCTAssertFalse(cellToDelete.exists, "Cell to delete exists after deletion")
+        // Terminate app
+        app.terminate()
+    }
+    
+    func testSwipeToDelete_deleteWithGesture() {
+        // Create and launch app
+        let app = XCUIApplication()
+        app.launch()
+        // Check if table exists
+        let table = app.tables.firstMatch
+        XCTAssertTrue(table.exists, "Table doesn't exist")
+        // Choose our cell to delete and check if exists
+        let cellToDelete = table/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"qui est esse")/*[[".cells.containing(.button, identifier:\"Delete\")",".cells.containing(.staticText, identifier:\"qui est esse\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch
+        XCTAssertTrue(cellToDelete.exists, "Cell to delete doesn't exist")
+        // Drag left to perform automatic delete
+        let rightOffset = CGVector(dx: 0.95, dy: 0.5)
+        let leftOffset = CGVector(dx: 0.05, dy: 0.5)
+        let cellFarRightCoordinate = cellToDelete.coordinate(withNormalizedOffset: rightOffset)
+        let cellFarLeftCoordinate = cellToDelete.coordinate(withNormalizedOffset: leftOffset)
+        cellFarRightCoordinate.press(forDuration: 0.1, thenDragTo: cellFarLeftCoordinate)
+        // Check if the cell was deleted
+        XCTAssertFalse(cellToDelete.exists, "Cell to delete exists after deletion")
+        // Terminate app
+        app.terminate()
+    }
+    
+    // MARK: DetailView
+    
+    func testDetailShowsRightInfo() {
+        // Create and launch app
+        let app = XCUIApplication()
+        app.launch()
+        // Check if table exists
+        let table = app.tables.firstMatch
+        XCTAssertTrue(table.exists, "Table doesn't exist")
+        // Choose our cell to check it's detail info
+        let cellToTap = table/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"qui est esse")/*[[".cells.containing(.button, identifier:\"Delete\")",".cells.containing(.staticText, identifier:\"qui est esse\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.firstMatch
+        XCTAssertTrue(cellToTap.exists, "Cell to tap doesn't exist")
+        cellToTap.tap()
+        // Check if we have the right info displayed
+        let titleLabel = app.staticTexts["qui est esse"]
+        XCTAssertTrue(titleLabel.exists, "Title is not showing")
+        // Use NSPredicate workaround to partially match the text
+        var textToMatch = "est rerum tempore vitae"
+        var predicate = NSPredicate(format: "label CONTAINS '\(textToMatch)'")
+        let bodyLabel = app.staticTexts.matching(predicate).firstMatch
+        XCTAssertTrue(bodyLabel.exists, "Body is not showing")
+        let userNameLabel = app.staticTexts["Leanne Graham"]
+        XCTAssertTrue(userNameLabel.waitForExistence(timeout: 2), "User-Name is not showing")
+        let userEmailLabel = app.staticTexts["Sincere@april.biz"]
+        XCTAssertTrue(userEmailLabel.waitForExistence(timeout: 2), "User-Email is not showing")
+        let userPhoneLabel = app.staticTexts["1-770-736-8031 x56442"]
+        XCTAssertTrue(userPhoneLabel.waitForExistence(timeout: 2), "User-Phone is not showing")
+        let userWebsiteLabel = app.staticTexts["hildegard.org"]
+        XCTAssertTrue(userWebsiteLabel.waitForExistence(timeout: 2), "User-Website is not showing")
+        textToMatch = "doloribus at sed quis culpa deserunt consectetur qui praesentium"
+        predicate = NSPredicate(format: "label CONTAINS '\(textToMatch)'")
+        let firstComment = app.tables.cells.containing(predicate).firstMatch
+        XCTAssertTrue(firstComment.waitForExistence(timeout: 2), "First comment is not showing")
+        // Terminate app
+        app.terminate()
     }
 }
